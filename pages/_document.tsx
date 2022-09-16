a284874816cfff139cfcd2dbefa2bec2152d3b45
RAREBEEF <drrobot409@gmail.com>
2022-09-16 12:34:33 +0900

    Fix Uncaught SyntaxError #5

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -106,6 +106,7 @@ const Document = () => {
         <base href="/" />
       </Head>
       <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
         <Main />
         <NextScript />
       </body>
---
+++ pages/_fallback.tsx
@@ -0,0 +1,7 @@
+const Fallback = () => (
+  <div>
+    <h1>This is fallback page when device is offline.</h1>
+  </div>
+);
+
+export default Fallback;
